8b3420edd7892e58f3baa3ed24c68c2dc639386e
Oleg <susakoleg105@gmail.com>
2026-06-05 16:06:24 +0200

    enable proxy headers for HTTPS static urls

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ echo "Running migrations..."
 alembic upgrade head
 
 echo "Starting server..."
-exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000} --proxy-headers --forwarded-allow-ips='*'
